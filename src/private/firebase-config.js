13cb6dd0679e21768c159cfdedc5d2ade71b7158
Marcos Bertilotti <bertilotti.marcos@gmail.com>
2020-10-20 18:58:53 -0300

    firebase config

---
+++ src/private/firebase-config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: 'API_KEY',
+  authDomain: 'AUTH_DOMAIN',
+  databaseURL: 'DB_URL',
+  projectId: 'ID',
+  storageBucket: 'STORAGE',
+  messagingSenderId: 'MESSAGEID',
+  appId: 'APPID',
+};
